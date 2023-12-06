2b1afc435209be0dd55255a065c2bd7defb3478e
Kauã <cagaxes@gmail.com>
2023-12-06 01:37:57 -0300

    novo

--- br/com/uninassau/Floresta/Coelho.java
+++
@@ -1,8 +0,0 @@
-package br.com.uninassau.floresta;
-
-public class Coelho extends Animal {
-	// Construtor para Coelho
-	public Coelho(int startX, int startY) {
-		super("Coelho", "Presa", 'C', 1, startX, startY);
-	}
-}
--- br/com/uninassau/Floresta/Veado.java
+++
@@ -1,9 +0,0 @@
-package br.com.uninassau.floresta;
-
-public class Veado extends Animal {
-
-	// Construtor para Veado
-	public Veado(int startX, int startY) {
-		super("Veado", "Presa", 'V', 3, startX, startY);
-	}
-}
